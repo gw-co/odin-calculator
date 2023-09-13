b289dbf8771751d1c3d262349b415ac1beac4026
gw-co <govindawaghmar@gmail.com>
2023-09-14 03:01:13 +0500

    properly functional now.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 let currentvalue = document.querySelector('.current-value');
 let prev = document.querySelector('.prev');
 let screenvalue = '';
-let ans = 0, num1 = 0, num2 = 0, operation = '';
+let ans = 0, num1 = 0, num2 = 0, operation = 'started';
 let isdecimal = false, isnegative = false;
+
+document.onkeydown = function(e){
+    let key = e.key;
+    switch (key){
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        case 'n':
+        case '.':
+        case '+': case '-': case '*': case '/': case '^':
+        case '=':
+            buttonfunc(key);
+            break;
+        case 'Enter':
+            buttonfunc('=');
+            break;
+        case 'Backspace':
+            buttonfunc('b');
+            break;
+        case 'Escape':
+            buttonfunc('A');
+        break;
+        default:
+            break;
+    }
+}
 
 function buttonfunc(v, thisbutton){
     // console.log(v);
+    if(operation = 'started'){
+        operation = '';
+        prev.textContent = '';
+    }
     switch (v){
         case '0': case '1': case '2': case '3': case '4':
         case '5': case '6': case '7': case '8': case '9':{
@@ -44,39 +73,27 @@ function buttonfunc(v, thisbutton){
         }
         case '+': case '-': case '*': case '/': case '^':{
             // console.log('operating  ' + screenvalue);
-            if(operation == ''){ // on startup or all clear
-                if(screenvalue == '') {return;}
-                else {
+            if( operation == '' || operation == '='){ // on startup or all clear
+                if(operation == '' && screenvalue == '') return;
+                if(screenvalue == '')
+                    num1 = ans;
+                else
                     num1 = Number(screenvalue);
-                    if(isNaN(num1)) num1 = 0;
-                    isdecimal = false;
-                    isnegative = false;
-                    operation = v;
-                    screenvalue = '';
-                    setscreenvalue('');
-                    prev.textContent = num1 + operation;
+                if(isNaN(num1) || !isFinite(num1)){
+                    num1 = 0;
                     return;
                 }
+                isdecimal = false;
+                isnegative = false;
+                operation = v;
+                screenvalue = '';
+                setscreenvalue('');
+                prev.textContent = num1 + ' ' + operation;
             }
             else{
-                if(operation == '='){ // on last time pressed =
-                    if(screenvalue == '')
-                        num1 = ans;
-                    else
-                        num1 = Number(screenvalue);
-                    if(isNaN(num1)) num1 = 0;
-                    isdecimal = false;
-                    isnegative = false;
-                    operation = v;
-                    screenvalue = '';
-                    setscreenvalue('');
-                    prev.textContent = num1 + operation;
-                    return;
-
-                }
                 if(screenvalue == ''){
                     operation = v;
-                    prev.textContent = num1 + operation;
+                    prev.textContent = num1 + ' ' + operation;
                 }
                 else{
                     num2 = Number(screenvalue);
@@ -88,21 +105,23 @@ function buttonfunc(v, thisbutton){
                     num1 = ans;
                     
                     operation = v;
-                    prev.textContent = num1 + operation;
-                    return;
+                    prev.textContent = num1 + ' ' + operation;
                 }
             }
+            return;
         }
         case '=':{
-            if(operation == ''|| screenvalue == '')return;
+            if(operation == '' || operation == '=' || screenvalue == '')return;
             num2 = Number(screenvalue);
             if(isNaN(num2)) num2 = 0;
             if(operation == '/' && num2 == 0){
-                setscreenvalue('not this one.')
-                return;
+                isdecimal = false;
+                isnegative = false;
+                prev.textContent = num1 + ' ' + operation + ' ' + num2;
+                setscreenvalue('not this one.');
             }
             else{
-                prev.textContent = num1 + operation + num2;
+                prev.textContent = num1 + ' ' + operation + ' ' + num2;
                 process();
                 setscreenvalue(ans.toString());
             }
@@ -113,6 +132,10 @@ function buttonfunc(v, thisbutton){
             return;
         }
         case 'b':{
+            if(operation == '='){
+                buttonfunc('A');
+                return;
+            }
             if(screenvalue.at(-1) == '.') isdecimal = false;
             if(screenvalue.at(-1) == '-') isnegative = false;
             screenvalue = screenvalue.slice(0, -1);
@@ -121,7 +144,6 @@ function buttonfunc(v, thisbutton){
         }
         case 'A':{
             //reset
-            // clearscreen();
             setscreenvalue('');
             prev.textContent = '';
             screenvalue = '';
@@ -135,25 +157,11 @@ function buttonfunc(v, thisbutton){
         }
     }
     if(operation == '=') prev.textContent = '';
-    // console.log('before ' + screenvalue);
-    // if(screenvalue == '' || screenvalue == '0'){
-    //     screenvalue = '';
-    //     isdecimal = false;
-    //     isnegative = false;
-    // }
-    // if(screenvalue == '-00'){
-    //     screenvalue = '-0';
-    // } 
-        
-    // if(screenvalue == '-.') screenvalue = '-0.';
-    // console.log('after  ' + screenvalue);//, Number('-01'));
-    // setscreenvalue(screenvalue);
 }
 
 function process(){
     if(screenvalue == '') return;
     if(screenvalue == '-') screenvalue = '0';
-    // console.log(num1, num2, operation);
     switch (operation){
         case '+':{
             ans = num1 + num2;
@@ -176,10 +184,7 @@ function process(){
             break;
         }
     }
-    // let fix = 1000000000;
-    // ans = Math.round(ans * fix) / fix;
-    console.log(num1, num2, operation, ans);
-    // num1 = ans;
+    // console.log(num1, num2, operation, ans);
     isdecimal = false;
     isnegative = false;
 }
@@ -188,7 +193,7 @@ function setscreenvalue(value){
     if(value.length > 20)
         currentvalue.style.fontSize = (800 / value.length).toString()+'px';
     else
-    currentvalue.style.fontSize = '40px';        
+        currentvalue.style.fontSize = '40px';        
     if(value == ''){
         currentvalue.textContent = '0';
     }
@@ -198,11 +203,4 @@ function setscreenvalue(value){
     else{
         currentvalue.textContent = value;
     }
-}
-
-function getnum(){
-    let n = Number(screenvalue);
-    if(isNaN(n)) n = 0;
-    isdecimal = false;
-    isnegative = false;
 }
